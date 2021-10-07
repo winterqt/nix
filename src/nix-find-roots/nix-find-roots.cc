48e1e30ac8b34440fca96043d54600de749c271e
regnat <rg@regnat.ovh>
2021-10-07 10:25:38 +0200

    Make the root-tracer directly listen on a socket
    Complicates the code quite a bit (compared to letting systemd handle
    that for us), but makes things much easier to test

--- src/nix-find-roots/nix-find-roots.cc
+++ src/nix-find-roots/nix-find-roots.cc
@@ -11,17 +11,36 @@
 #include <iostream>
 #include <regex>
 #include <set>
+#include <unistd.h>
 #include <vector>
 #include <algorithm>
 #include <getopt.h>
 #include <fstream>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 namespace fs = std::filesystem;
 using std::set, std::string;
 
+class Error : public std::exception {
+private:
+    const string message;
+
+public:
+    Error(std::string message)
+        : message(message)
+    {}
+
+    const char* what() const noexcept override
+    {
+        return message.c_str();
+    }
+};
+
 struct GlobalOpts {
     fs::path storeDir = "/nix/store";
     fs::path stateDir = "/nix/var/nix";
+    fs::path socketPath = "/nix/var/nix/gc-socket/socket";
     enum VerbosityLvl {
         Quiet,
         Verbose
@@ -40,18 +59,19 @@ GlobalOpts parseCmdLine(int argc, char** argv)
 {
     GlobalOpts res;
     auto usage = [&]() {
-        std::cerr << "Usage: " << string(argv[0]) << " [--verbose|-v] [-s storeDir] [-d stateDir]" << std::endl;
+        std::cerr << "Usage: " << string(argv[0]) << " [--verbose|-v] [-s storeDir] [-d stateDir] [-l socketPath]" << std::endl;
         exit(1);
     };
     static struct option long_options[] = {
         { "verbose", no_argument, (int*)&res.verbosity, GlobalOpts::Verbose },
+        { "socket_path", required_argument, 0, 'l' },
         { "store_dir", required_argument, 0, 's' },
         { "state_dir", required_argument, 0, 'd' },
     };
 
     int option_index = 0;
     int opt_char;
-    while((opt_char = getopt_long(argc, argv, "vs:",
+    while((opt_char = getopt_long(argc, argv, "vsl:",
                     long_options, &option_index)) != -1) {
         switch (opt_char) {
             case 0:
@@ -68,6 +88,9 @@ GlobalOpts parseCmdLine(int argc, char** argv)
                 break;
             case 'd':
                 res.stateDir = fs::path(optarg);
+                break;
+            case 'l':
+                res.socketPath = fs::path(optarg);
                 break;
             default:
                 std::cerr << "Got invalid char: " << (char)opt_char << std::endl;
@@ -296,21 +319,62 @@ Roots getRuntimeRoots(GlobalOpts opts)
 
 int main(int argc, char * * argv)
 {
-    GlobalOpts opts = parseCmdLine(argc, argv);
-    set<fs::path> standardRoots = {
+    const GlobalOpts opts = parseCmdLine(argc, argv);
+    const set<fs::path> standardRoots = {
         opts.stateDir / fs::path("profiles"),
         opts.stateDir / fs::path("gcroots"),
     };
-    auto traceResult = followPathsToStore(opts, standardRoots);
-    auto runtimeRoots = getRuntimeRoots(opts);
-    traceResult.storeRoots.insert(runtimeRoots.begin(), runtimeRoots.end());
-    for (auto & [rootInStore, externalRoots] : traceResult.storeRoots) {
-        for (auto & externalRoot : externalRoots)
-            std::cout << rootInStore.string() << '\t' << externalRoot.string() << std::endl;
+
+    int mySock = socket(PF_UNIX, SOCK_STREAM, 0);
+    if (mySock == 0) {
+        throw Error("Cannot create Unix domain socket");
     }
-    std::cout << std::endl;
-    for (auto & deadLink : traceResult.deadLinks) {
-        std::cout << deadLink.string() << std::endl;
+    struct sockaddr_un addr;
+    addr.sun_family = AF_UNIX;
+
+    unlink(opts.socketPath.c_str());
+    strcpy(addr.sun_path, opts.socketPath.c_str());
+    if (bind(mySock, (struct sockaddr*) &addr, sizeof(addr)) == -1) {
+        throw Error("Cannot bind to socket");
+    }
+
+    if (listen(mySock, 5) == -1)
+        throw Error("cannot listen on socket " + opts.socketPath.string());
+
+    addr.sun_family = AF_UNIX;
+    while (1) {
+        struct sockaddr_un remoteAddr;
+        socklen_t remoteAddrLen = sizeof(remoteAddr);
+        int remoteSocket = accept(
+                mySock,
+                (struct sockaddr*) & remoteAddr,
+                &remoteAddrLen
+                );
+
+        if (remoteSocket == -1) {
+            if (errno == EINTR) continue;
+            throw Error("Error accepting the connection");
+        }
+
+        auto traceResult = followPathsToStore(opts, standardRoots);
+        auto runtimeRoots = getRuntimeRoots(opts);
+        traceResult.storeRoots.insert(runtimeRoots.begin(), runtimeRoots.end());
+        for (auto & [rootInStore, externalRoots] : traceResult.storeRoots) {
+            for (auto & externalRoot : externalRoots) {
+                send(remoteSocket, rootInStore.string().c_str(), rootInStore.string().size(), 0);
+                send(remoteSocket, "\t", strlen("\t"), 0);
+                send(remoteSocket, externalRoot.string().c_str(), externalRoot.string().size(), 0);
+                send(remoteSocket, "\n", strlen("\n"), 0);
+            }
+
+        }
+        send(remoteSocket, "\n", strlen("\n"), 0);
+        for (auto & deadLink : traceResult.deadLinks) {
+            send(remoteSocket, deadLink.string().c_str(), deadLink.string().size(), 0);
+            send(remoteSocket, "\n", strlen("\n"), 0);
+        }
+
+        close(remoteSocket);
     }
 }
 
